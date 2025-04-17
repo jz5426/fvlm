6c2e56f629ec9fb42dcc088f12df17278970c29a
jz5426 <max.you@outlook.com>
2025-04-17 18:42:43 -0400

    progress on generate mask

--- data/fix_data.py
+++ data/fix_data.py
@@ -7,23 +7,38 @@ import numpy as np
 import pandas as pd
 import SimpleITK as sitk
 import tqdm
-
+import h5py
 
 def process_row(row):
     # Set up directory parameters
     VolumeName = row["VolumeName"]
     dir1 = VolumeName.rsplit("_", 1)[0]
     dir2 = VolumeName.rsplit("_", 2)[0]
-    filepath = os.path.join(data_root, f"{split}", dir2, dir1, VolumeName)
+    #TODO: organize the files in the following fomrat if necessary.
+    # filepath = os.path.join(data_root, f"{split}", dir2, dir1, VolumeName)
 
+    # transform from "train_1_a" to "train_1a" NOTE:TODO: this is just temporary changes, the file structure should follows exactly the same as the one from metadata file ideally.
+    dir1 = dir1[::-1].replace("_", "", 1)[::-1] 
+    # Handle compound extensions like .nii.gz
+    base, ext = os.path.splitext(VolumeName)
+    if ext == ".gz":
+        base, _ = os.path.splitext(base)  # strip .nii as well
+    VolumeName = base + ".h5"
+    filepath = os.path.join(f'/cluster/projects/mcintoshgroup/publicData/CT-RATE-Processed/benchmark/CTRATE_Volumes_raw_h5_fp16_noflip_{split}', dir2, dir1, VolumeName)
     dirpath = os.path.dirname(filepath)
-    dirpath = dirpath.replace(f"/{split}/", f"/{split}_fixed/")
+    dirpath = dirpath.replace(f"/CTRATE_Volumes_raw_h5_fp16_noflip_{split}/", f"/CTRATE_Volumes_raw_h5_fp16_noflip_{split}_fixed/")
 
     if os.path.exists(os.path.join(dirpath, os.path.basename(filepath))):
         return
 
     # Read Image
-    image = sitk.ReadImage(filepath)
+    if filepath.endswith('h5'):
+        with h5py.File(filepath, "r") as f:
+            image_np = f["ct"][:].astype(np.float32) # a numpy array (512, 512, 303), which is the original shape, by convention it is x,y,z
+            image_np = np.transpose(image_np, (2, 1, 0)) # become z, y, x
+        image = sitk.GetImageFromArray(image_np)  # NOTE: This assumes axis order is z, y, x but image is in axis order of x, y, z
+
+    # image = sitk.ReadImage(filepath) # assume axis order of x, y, z
 
     # Set Spacing
     (x, y), z = map(float, ast.literal_eval(row["XYSpacing"])), row["ZSpacing"]
@@ -48,22 +63,34 @@ def process_row(row):
 
     # Write Image
     Path(dirpath).mkdir(parents=True, exist_ok=True)
-    sitk.WriteImage(adjusted_hu, os.path.join(dirpath, os.path.basename(filepath)))
+
+    # replace
+    # NOTE: totalsegmentor requires nifitimage file format to do the the segmentation
+    base, ext = os.path.splitext(filepath)
+    base += '.nii.gz'
+    # np.transpose(np_image_transposed, (1, 2, 0)) # reverse the previous transpose operation
+    sitk.WriteImage(adjusted_hu, os.path.join(dirpath, os.path.basename(base)))
 
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
 
-    parser.add_argument("--split", required=False, default='train', type='str')
-    args = parser.parse_args()
-    
-    split = args.split
+    # /cluster/projects/mcintoshgroup/publicData/CT-RATE-Processed/benchmark/original/train_1_a_1.nii.gz
+    # img = sitk.ReadImage('/cluster/projects/mcintoshgroup/publicData/CT-RATE-Processed/benchmark/original/train_1_a_1.nii.gz') # in shape (512, 512, 303)
+
+    # import argparse
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--split", required=False, default='train', type='str')
+    # args = parser.parse_args()
+    # split = args.split
+
+    split = 'train'
     d = "validation" if split == "valid" else "train"
     
-    data_root = Path("")
+    data_root = Path("/cluster/projects/mcintoshgroup/publicData/CT-RATE/dataset/")
     metadata = pd.read_csv(os.path.join(data_root, f"metadata/{d}_metadata.csv"))
     rows = [row[1] for row in metadata.iterrows()]
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        list(tqdm.tqdm(executor.map(process_row, rows), total=len(rows)))
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     list(tqdm.tqdm(executor.map(process_row, rows), total=len(rows)))
+    process_row(rows[0])
+    print('finished')
--- data/generate_mask.py
+++ data/generate_mask.py
@@ -1,20 +1,28 @@
 import os
+import sys
 from pathlib import Path
-
 import nibabel as nib
 import pandas as pd
-from totalsegmentator.python_api import totalsegmentator
-
+if os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) not in sys.path:
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from data.TotalSegmentator.totalsegmentator.python_api import totalsegmentator
 
 def generate_mask(row):
     VolumeName = row["VolumeName"]
     dir1 = VolumeName.rsplit("_", 1)[0]
     dir2 = VolumeName.rsplit("_", 2)[0]
-    filepath = os.path.join(data_root, f"{split}_fixed", dir2, dir1, VolumeName)
 
+    # filepath = os.path.join(data_root, f"{split}_fixed", dir2, dir1, VolumeName)
+    # dirpath = os.path.dirname(filepath)
+    # dirpath = dirpath.replace(f"/{split}_fixed/", f"/{split}_mask/")
+
+    # transform from "train_1_a" to "train_1a" NOTE:TODO: this is just temporary changes, the file structure should follows exactly the same as the one from metadata file ideally.
+    dir1 = dir1[::-1].replace("_", "", 1)[::-1] 
+    filepath = os.path.join(f'/cluster/projects/mcintoshgroup/publicData/CT-RATE-Processed/benchmark/CTRATE_Volumes_raw_h5_fp16_noflip_{split}_fixed', dir2, dir1, VolumeName)
     dirpath = os.path.dirname(filepath)
-    dirpath = dirpath.replace(f"/{split}_fixed/", f"/{split}_mask/")
-    
+    dirpath = dirpath.replace(f"/CTRATE_Volumes_raw_h5_fp16_noflip_{split}_fixed/", f"/CTRATE_Volumes_raw_h5_fp16_noflip_{split}_mask/")
+
+    # NOTE: extension should be .nii.gz, not the h5 version.
     input_img = nib.load(filepath)
     output_img = totalsegmentator(input_img, quiet=True)
 
@@ -23,16 +31,17 @@ def generate_mask(row):
 
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="Training")
+    # import argparse
+    # parser = argparse.ArgumentParser(description="Training")
+    # parser.add_argument("--split", required=False, default='train', type='str')
+    # args = parser.parse_args()
+    # split = args.split
 
-    parser.add_argument("--split", required=False, default='train', type='str')
-    args = parser.parse_args()
-    
-    split = args.split
+    split = 'train'
     d = "validation" if split == "valid" else "train"
-    data_root = Path()
+    data_root = Path("/cluster/projects/mcintoshgroup/publicData/CT-RATE/dataset/")
     metadata = pd.read_csv(os.path.join(data_root, f"metadata/{d}_metadata.csv"))
     rows = [row[1] for row in metadata.iterrows()]
-    for row in rows:
-        generate_mask(row)
+    # for row in rows:
+    #     generate_mask(row)
+    generate_mask(rows[0])
